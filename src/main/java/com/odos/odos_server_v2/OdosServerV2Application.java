20e077068a8d0612fe9296d00e8c463b57bc483f
epdlrnldudnj <kje0023@naver.com>
2025-07-27 23:30:12 +0900

    Initial commit

---
+++ src/main/java/com/odos/odos_server_v2/OdosServerV2Application.java
@@ -0,0 +1,13 @@
+package com.odos.odos_server_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdosServerV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdosServerV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/odos/odos_server_v2/OdosServerV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odos.odos_server_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdosServerV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
