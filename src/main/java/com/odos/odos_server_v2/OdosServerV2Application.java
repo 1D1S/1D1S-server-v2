4ff2bdaad2090f8ec1da23c7519f41fa07a59435
epdlrnldudnj <kje0023@naver.com>
2025-07-28 17:58:05 +0900

    Chore : Spotless 적용

--- src/main/java/com/odos/odos_server_v2/OdosServerV2Application.java
+++ src/main/java/com/odos/odos_server_v2/OdosServerV2Application.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OdosServerV2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OdosServerV2Application.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(OdosServerV2Application.class, args);
+  }
 }
--- src/main/java/com/odos/odos_server_v2/config/SwaggerConfig.java
+++ src/main/java/com/odos/odos_server_v2/config/SwaggerConfig.java
@@ -10,26 +10,27 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-    @Bean
-    public OpenAPI openAPI() {
-        String jwt = "JWT";
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
-        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
-                .name(jwt)
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT")
-        );
-        return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo())
-                .addSecurityItem(securityRequirement)
-                .components(components);
-    }
-    private Info apiInfo() {
-        return new Info()
-                .title("1D1S server-v2")
-                .description("1d1s 백엔드 api 테스트 입니다")
-                .version("2.0.0");
-    }
+  @Bean
+  public OpenAPI openAPI() {
+    String jwt = "JWT";
+    SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+    Components components =
+        new Components()
+            .addSecuritySchemes(
+                jwt,
+                new SecurityScheme()
+                    .name(jwt)
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT"));
+    return new OpenAPI()
+        .components(new Components())
+        .info(apiInfo())
+        .addSecurityItem(securityRequirement)
+        .components(components);
+  }
+
+  private Info apiInfo() {
+    return new Info().title("1D1S server-v2").description("1d1s 백엔드 api 테스트 입니다").version("2.0.0");
+  }
 }
