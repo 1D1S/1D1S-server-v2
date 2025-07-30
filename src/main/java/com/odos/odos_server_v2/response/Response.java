9e4b2cbe77f459b2e2b2c0c5f325d24d02ad1ed8
epdlrnldudnj <kje0023@naver.com>
2025-07-30 15:52:29 +0900

    Feat : Response 클래스 구현

---
+++ src/main/java/com/odos/odos_server_v2/response/Response.java
@@ -0,0 +1,26 @@
+package com.odos.odos_server_v2.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Response {
+    private String message;
+    private Object data;
+
+    public Response(String msg){
+        this.message = msg;
+    }
+
+    public static Response success(String msg){
+        return new Response(msg);
+    }
+    public static Response success(String msg, Object data){
+        return new Response(msg, data);
+    }
+
+    public static Response failure(String msg){
+        return new Response(msg);
+    }
+}
