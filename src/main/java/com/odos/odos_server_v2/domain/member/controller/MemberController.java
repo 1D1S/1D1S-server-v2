04de0a3f533178444201d186e94f9f1016b931e5
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2025-07-31 05:03:42 +0900

    Feat : Member Domain base code 개발

--- src/main/java/com/odos/odos_server_v2/domain/member/controller/MemberController.java
+++ src/main/java/com/odos/odos_server_v2/domain/member/controller/MemberController.java
@@ -1,3 +1,8 @@
 package com.odos.odos_server_v2.domain.member.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class MemberController {}
--- src/main/java/com/odos/odos_server_v2/domain/member/repository/MemberRepository.java
+++ src/main/java/com/odos/odos_server_v2/domain/member/repository/MemberRepository.java
@@ -1,3 +1,8 @@
 package com.odos.odos_server_v2.domain.member.repository;
 
-public interface MemberRepository {}
+import com.odos.odos_server_v2.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {}
