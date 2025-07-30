edc7e96a9d1f62bb0cd41e1a9d606c8ef0524cf9
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2025-07-31 05:55:41 +0900

    Feat : Shared Domain base code 개발

--- src/main/java/com/odos/odos_server_v2/domain/shared/service/CursorService.java
+++ src/main/java/com/odos/odos_server_v2/domain/shared/service/CursorService.java
@@ -1,3 +1,19 @@
 package com.odos.odos_server_v2.domain.shared.service;
 
-public class CursorService {}
+import java.util.Base64;
+
+public class CursorService {
+  public String encodeCursor(Long id) {
+    return Base64.getEncoder().encodeToString(("cursor:" + id).getBytes());
+  }
+
+  public Long decodeCursorToId(String cursor) {
+    if (cursor == null) return null;
+    try {
+      String decoded = new String(Base64.getDecoder().decode(cursor));
+      return Long.parseLong(decoded.split(":")[1]);
+    } catch (Exception e) {
+      return null;
+    }
+  }
+}
