fd78454f2464968ff91fd6c65f3c28cfa5cf1067
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2025-07-31 23:25:51 +0900

    Refactor : @Table 어노테이션 빠진 파일에 추가 및 MEMBER 테이블명 Member로 변경

--- src/main/java/com/odos/odos_server_v2/domain/member/entity/Member.java
+++ src/main/java/com/odos/odos_server_v2/domain/member/entity/Member.java
@@ -24,7 +24,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-@Table(name = "MEMBER")
+@Table(name = "Member")
 public class Member {
 
   @Id
