da4f99cce2118675f1e123f29da7fd9457714ae0
epdlrnldudnj <kje0023@naver.com>
2025-08-10 22:00:51 +0900

    Feat : LikeDto 생성

---
+++ src/main/java/com/odos/odos_server_v2/domain/shared/dto/LikeDto.java
@@ -0,0 +1,13 @@
+package com.odos.odos_server_v2.domain.shared.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LikeDto {
+  private boolean likedByMe;
+  private int likeCnt;
+}
