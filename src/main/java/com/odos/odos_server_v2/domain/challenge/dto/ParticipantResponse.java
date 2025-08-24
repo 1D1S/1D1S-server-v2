6040007bfb4106ff37f10314894a90b24ad0a744
epdlrnldudnj <kje0023@naver.com>
2025-08-24 23:44:52 +0900

    Rename : Participant 관련 이름 수정 ( Dto -> Response)

--- src/main/java/com/odos/odos_server_v2/domain/challenge/dto/ParticipantResponse.java
+++ src/main/java/com/odos/odos_server_v2/domain/challenge/dto/ParticipantResponse.java
@@ -3,10 +3,12 @@ package com.odos.odos_server_v2.domain.challenge.dto;
 import com.odos.odos_server_v2.domain.challenge.entity.Enum.ParticipantStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
 @AllArgsConstructor
-public class ParticipantDto {
+@Getter
+public class ParticipantResponse {
   private Long memberId;
   private Long participantId;
   private String nickname;
