d1fe38da8517f67429b77d38f1091b812a6939f9
epdlrnldudnj <kje0023@naver.com>
2025-08-24 23:54:33 +0900

    Feat : Challenge 생성, 신청, 수락, 불러오기 컨트롤러 개발

--- src/main/java/com/odos/odos_server_v2/domain/challenge/controller/ChallengeController.java
+++ src/main/java/com/odos/odos_server_v2/domain/challenge/controller/ChallengeController.java
@@ -1,8 +1,50 @@
 package com.odos.odos_server_v2.domain.challenge.controller;
 
+import com.odos.odos_server_v2.domain.challenge.dto.ChallengeRequest;
+import com.odos.odos_server_v2.domain.challenge.dto.ChallengeResponse;
+import com.odos.odos_server_v2.domain.challenge.dto.ChallengeSummaryResponse;
+import com.odos.odos_server_v2.domain.challenge.dto.ParticipantResponse;
+import com.odos.odos_server_v2.domain.challenge.service.ChallengeService;
+import com.odos.odos_server_v2.domain.member.CurrentUserContext;
+import com.odos.odos_server_v2.response.ApiResponse;
+import com.odos.odos_server_v2.response.Message;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-public class ChallengeController {}
+@RequestMapping("/challenges")
+public class ChallengeController {
+  private final ChallengeService challengeService;
+
+  @PostMapping
+  public ApiResponse<ChallengeSummaryResponse> createChallenge(
+      @RequestBody ChallengeRequest challengeRequest) {
+    Long memberId = CurrentUserContext.getCurrentMemberId();
+    return ApiResponse.success(
+        Message.CREATE_CHALLENGE, challengeService.createChallenge(challengeRequest, memberId));
+  }
+
+  @GetMapping("/{challengeId}")
+  public ApiResponse<ChallengeResponse> getChallenge(@PathVariable Long challengeId) {
+    Long memberId = CurrentUserContext.getCurrentMemberId();
+    return ApiResponse.success(
+        Message.GET_CHALLENGE, challengeService.getChallenge(challengeId, memberId));
+  }
+
+  @PostMapping("/{challengeId}/participants")
+  public ApiResponse<ParticipantResponse> applyParticipation(
+      @PathVariable Long challengeId, @RequestBody List<String> goals) {
+    Long memberId = CurrentUserContext.getCurrentMemberId();
+    return ApiResponse.success(
+        Message.APPLY_CHALLENGE, challengeService.applyParticipant(challengeId, memberId, goals));
+  }
+
+  @PatchMapping("/participants/{participantId}/accept")
+  public ApiResponse<Void> acceptParticipant(@PathVariable Long participantId) {
+    Long memberId = CurrentUserContext.getCurrentMemberId();
+    challengeService.acceptParticipant(participantId, memberId);
+    return ApiResponse.success(Message.ACCEPT_PARTICIPANT);
+  }
+}
