619d9e164e4389e4f48c0da776eb3cd937f863e8
epdlrnldudnj <kje0023@naver.com>
2025-08-24 23:43:46 +0900

    Feat : 성공 및 에러 메시지 추가

--- src/main/java/com/odos/odos_server_v2/response/Message.java
+++ src/main/java/com/odos/odos_server_v2/response/Message.java
@@ -7,4 +7,17 @@ public class Message {
   public static String TOKEN_REFRESH = "토큰 재발급 성공했습니다.";
   public static String LOGOUT = "로그아웃 성공했습니다.";
   public static String LOGIN_SUCCESS = "소셜 로그인 연결 성공했습니다.";
+
+  // challenge
+  public static String CREATE_CHALLENGE = "챌린지 생성 성공했습니다.";
+  public static String GET_CHALLENGE = "챌린지 상세 조회 성공했습니다.";
+  public static String APPLY_CHALLENGE = "챌린지 신청 성공했습니다.";
+  public static String ACCEPT_PARTICIPANT = "참여자 수락 성공했습니다.";
+  public static String REJECT_PARTICIPANT = "참여자 거절 성공했습니다.";
+  public static String GET_RANDOM_CHALLENGES = "챌린지 랜덤 불러오기 성공했습니다.";
+  public static String DELETE_PARTICIPANT = "챌린지 탈퇴하기 성공했습니다.";
+  public static String GET_CHALLENGE_LIST = "챌린지 리스트 불러오기 성공했습니다.";
+  public static String ADD_CHALLENGE_LIKE = "챌린지 좋아요 성공했습니다.";
+  public static String CANCEL_CHALLENGE_LIKE = "챌린지 좋아요 취소 성공했습니다.";
+  public static String GET_CHALLENGES_BY_MEMBER = "진행중인 챌린지 불러오기 성공했습니다.";
 }
