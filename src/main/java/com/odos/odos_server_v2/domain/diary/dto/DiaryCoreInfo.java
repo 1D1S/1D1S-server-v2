2740137f6c462321fdfeebc50c635a53befc3ef4
hwnooy <heejudgit@gmail.com>
2025-10-01 21:42:32 +0900

    Feat : 마이페이지 조회용 diaryCoreInfo dto 생성

---
+++ src/main/java/com/odos/odos_server_v2/domain/diary/dto/DiaryCoreInfo.java
@@ -0,0 +1,20 @@
+package com.odos.odos_server_v2.domain.diary.dto;
+
+import com.odos.odos_server_v2.domain.diary.entity.Enum.Feeling;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class DiaryCoreInfo {
+  private Long id;
+  private Long challengeId;
+  private String title;
+  private long likeCnt;
+  private String imgUrl;
+  private Boolean isPublic;
+  private String createdAt;
+  private String challengedDate;
+  private Feeling feeling;
+  private int achievement;
+}
