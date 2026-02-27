27f2c0e5c3949a8fdab6592e78f19761611df7eb
epdlrnldudnj <kje0023@naver.com>
2026-02-27 21:02:18 +0900

    feat: 성공 메시지 추가

--- src/main/java/com/odos/odos_server_v2/response/Message.java
+++ src/main/java/com/odos/odos_server_v2/response/Message.java
@@ -34,4 +34,8 @@ public class Message {
   public static String ADD_CHALLENGE_LIKE = "챌린지 좋아요 성공했습니다.";
   public static String CANCEL_CHALLENGE_LIKE = "챌린지 좋아요 취소 성공했습니다.";
   public static String GET_CHALLENGES_BY_MEMBER = "진행중인 챌린지 불러오기 성공했습니다.";
+
+  // member
+  public static String GET_MYPAGE = "마이페이지 조회 성공했습니다.";
+  public static String GET_SIDEBAR = "사이드바 조회 성공했습니다.";
 }
