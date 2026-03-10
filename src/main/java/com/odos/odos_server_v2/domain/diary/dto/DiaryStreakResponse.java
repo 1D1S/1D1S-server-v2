7c06617335b3f9a31304a87969b504b94f4f4bc1
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2026-03-10 19:08:16 +0900

    feat: 기획에 맞게 작성된 날짜 리스트로만 리턴되게 수정

--- src/main/java/com/odos/odos_server_v2/domain/diary/dto/DiaryStreakResponse.java
+++ src/main/java/com/odos/odos_server_v2/domain/diary/dto/DiaryStreakResponse.java
@@ -2,21 +2,17 @@ package com.odos.odos_server_v2.domain.diary.dto;
 
 import com.odos.odos_server_v2.domain.diary.entity.Diary;
 import java.time.LocalDate;
+import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class DiaryStreakResponse {
-  private Long diaryId;
-  private String diaryTitle;
-  private LocalDate diaryCreatedDate;
+  private List<LocalDate> diaryCreatedDate;
 
-  public DiaryStreakResponse checkStreak(Diary diary) {
-    return DiaryStreakResponse.builder()
-        .diaryId(diary.getId())
-        .diaryTitle(diary.getTitle())
-        .diaryCreatedDate(diary.getCreatedDate())
-        .build();
+  public static DiaryStreakResponse checkStreak(List<Diary> diary) {
+    List<LocalDate> diaryCreatedList = diary.stream().map(Diary::getCreatedDate).toList();
+    return DiaryStreakResponse.builder().diaryCreatedDate(diaryCreatedList).build();
   }
 }
