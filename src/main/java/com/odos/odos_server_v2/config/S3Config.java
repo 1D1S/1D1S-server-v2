8a7d5213398aa2c742276f3f0253edc55fc1f4a4
epdlrnldudnj <kje0023@naver.com>
2026-03-14 19:43:13 +0900

    feat: presigned url s3config 추가

--- src/main/java/com/odos/odos_server_v2/config/S3Config.java
+++ src/main/java/com/odos/odos_server_v2/config/S3Config.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 @RequiredArgsConstructor
@@ -12,5 +13,10 @@ public class S3Config {
   @Bean
   public S3Client s3Client() {
     return S3Client.builder().region(Region.AP_NORTHEAST_2).build();
+  }
+
+  @Bean
+  public S3Presigner s3Presigner() {
+    return S3Presigner.builder().region(Region.AP_NORTHEAST_2).build();
   }
 }
