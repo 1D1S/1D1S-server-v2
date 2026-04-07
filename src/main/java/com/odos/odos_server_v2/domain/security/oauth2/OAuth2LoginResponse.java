b9d1ef5ae7e9dd5ec733d942c5deea95b868fdd4
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2026-04-07 20:50:14 +0900

    refactor: accessToken과 refreshToken 제거

--- src/main/java/com/odos/odos_server_v2/domain/security/oauth2/OAuth2LoginResponse.java
+++ src/main/java/com/odos/odos_server_v2/domain/security/oauth2/OAuth2LoginResponse.java
@@ -10,7 +10,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class OAuth2LoginResponse {
-  private String accessToken;
-  private String refreshToken;
   private boolean isProfileComplete;
 }
