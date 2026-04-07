11f7a50390a40a4f8156bc3a1becde2cac849048
epdlrnldudnj <kje0023@naver.com>
2026-04-07 21:11:36 +0900

    feat: 댓글 관련 controller 개발

--- src/main/java/com/odos/odos_server_v2/config/SecurityConfig.java
+++ src/main/java/com/odos/odos_server_v2/config/SecurityConfig.java
@@ -46,6 +46,8 @@ public class SecurityConfig {
                     .permitAll()
                     .requestMatchers(HttpMethod.GET, "/challenges/**")
                     .permitAll()
+                    .requestMatchers(HttpMethod.GET, "/comments/**")
+                    .permitAll()
                     .requestMatchers(
                         "/",
                         "/auth/**",
