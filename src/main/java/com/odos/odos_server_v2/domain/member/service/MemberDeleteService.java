ccbf03d4442d41c310ff90da3b769ab45c6a7551
nayoung16 <nykim1016@sookmyung.ac.kr>
2026-04-17 21:38:31 +0900

    feat: hard delete 로직 제거

--- src/main/java/com/odos/odos_server_v2/domain/member/service/MemberDeleteService.java
+++ src/main/java/com/odos/odos_server_v2/domain/member/service/MemberDeleteService.java
@@ -1,14 +1,15 @@
 package com.odos.odos_server_v2.domain.member.service;
 
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.odos.odos_server_v2.domain.challenge.service.ChallengeService;
 import com.odos.odos_server_v2.domain.member.CurrentUserContext;
 import com.odos.odos_server_v2.domain.member.entity.Member;
 import com.odos.odos_server_v2.domain.member.repository.MemberRepository;
-import java.time.LocalDateTime;
-import java.util.List;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -30,26 +31,9 @@ public class MemberDeleteService {
     member.withdraw();
 
     // 주최 중인 챌린지 host 위임
-    challengeService.withdrawMemberLeaveChallengeHost(member.getId());
-  }
+    challengeService.withdrawMemberLeaveChallenge(member.getId());
 
-  /** 2. 실제 삭제 (Hard Delete) - 탈퇴 후 7일 지난 회원 */
-  @Transactional
-  public void hardDelete(Member member) {
-    memberRepository.delete(member);
-  }
-
-  /** 3. 스케줄러에서 실행 */
-  @Transactional
-  public void processDeletion() {
-
-    LocalDateTime threshold = LocalDateTime.now().minusDays(7);
-
-    List<Member> targets = memberRepository.findDeletableMembers(threshold);
-
-    for (Member member : targets) {
-      hardDelete(member);
-    }
+    // 일지 처리
   }
 
   @Transactional
@@ -65,7 +49,5 @@ public class MemberDeleteService {
 
     // 주최 중인 챌린지 host 위임
     challengeService.withdrawMemberLeaveChallengeHost(member.getId());
-
-    hardDelete(member);
   }
 }
