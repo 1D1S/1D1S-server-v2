6b9bbeed0b63b33e105d3e63d4504024481031e7
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2026-04-28 14:30:52 +0900

    feat: Notification 관련 ENUM 추가

---
+++ src/main/java/com/odos/odos_server_v2/domain/notification/entity/Enum/NotificationTargetType.java
@@ -0,0 +1,11 @@
+package com.odos.odos_server_v2.domain.notification.entity.Enum;
+
+public enum NotificationTargetType {
+  MEMBER_PROFILE,
+  DIARY_DETAIL,
+  DIARY_COMMENT,
+  CHALLENGE_DETAIL,
+  CHALLENGE_LIST,
+  MAIN
+}
+    // TODO : 이거 타입 이름과 기획 다시 비교
---
+++ src/main/java/com/odos/odos_server_v2/domain/notification/entity/Enum/NotificationType.java
@@ -0,0 +1,12 @@
+package com.odos.odos_server_v2.domain.notification.entity.Enum;
+
+public enum NotificationType {
+  FRIEND_REQUEST,
+  FRIEND_ACCEPT,
+  FRIEND_DIARY_CREATED,
+  MY_DIARY_COMMENTED,
+  MY_COMMENT_REPLIED,
+  DIARY_LIKE_MILESTONE,
+  CHALLENGE_APPROVED,
+  CHALLENGE_REJECTED
+}
