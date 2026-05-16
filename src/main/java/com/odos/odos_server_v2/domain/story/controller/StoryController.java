940ce15591fab60abc2310a9748ac7d6ba01f121
nayoung16 <nykim1016@sookmyung.ac.kr>
2026-05-16 15:04:19 +0900

    chore: spotlessApply

--- src/main/java/com/odos/odos_server_v2/domain/story/controller/StoryController.java
+++ src/main/java/com/odos/odos_server_v2/domain/story/controller/StoryController.java
@@ -1,9 +1,5 @@
 package com.odos.odos_server_v2.domain.story.controller;
 
-import lombok.RequiredArgsConstructor;
-
-import org.springframework.web.bind.annotation.*;
-
 import com.odos.odos_server_v2.domain.story.dto.StoryResponseDto;
 import com.odos.odos_server_v2.domain.story.service.StoryService;
 import com.odos.odos_server_v2.response.ApiResponse;
@@ -13,6 +9,8 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "스토리", description = "실시간 일지(스토리) API")
 @RestController
