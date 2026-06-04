eb9ef0e12acae3cb25b6e7f2057d5cefaa881484
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2026-06-05 01:44:27 +0900

    feat: AsyncConfig 추가

---
+++ src/main/java/com/odos/odos_server_v2/config/AsyncConfig.java
@@ -0,0 +1,23 @@
+package com.odos.odos_server_v2.config;
+
+import java.util.concurrent.Executor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+  @Bean(name = "notificationTaskExecutor")
+  public Executor notificationTaskExecutor() {
+    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+    executor.setCorePoolSize(4);
+    executor.setMaxPoolSize(16);
+    executor.setQueueCapacity(500);
+    executor.setThreadNamePrefix("notification-push-");
+    executor.initialize();
+    return executor;
+  }
+}
