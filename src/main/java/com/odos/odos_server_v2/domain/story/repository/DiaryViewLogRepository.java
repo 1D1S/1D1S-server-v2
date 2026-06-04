fc4bac615b087cf2a95e3ef71cb1e2e269e7eb2d
nayoung16 <nykim1016@sookmyung.ac.kr>
2026-06-04 22:35:09 +0900

    chore: spotlessApply

--- src/main/java/com/odos/odos_server_v2/domain/story/repository/DiaryViewLogRepository.java
+++ src/main/java/com/odos/odos_server_v2/domain/story/repository/DiaryViewLogRepository.java
@@ -1,11 +1,9 @@
 package com.odos.odos_server_v2.domain.story.repository;
 
+import com.odos.odos_server_v2.domain.story.entity.DiaryViewLog;
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.odos.odos_server_v2.domain.story.entity.DiaryViewLog;
 
 @Repository
 public interface DiaryViewLogRepository extends JpaRepository<DiaryViewLog, Long> {
--- src/main/java/com/odos/odos_server_v2/domain/story/repository/StoryRepository.java
+++ src/main/java/com/odos/odos_server_v2/domain/story/repository/StoryRepository.java
@@ -1,15 +1,13 @@
 package com.odos.odos_server_v2.domain.story.repository;
 
+import com.odos.odos_server_v2.domain.diary.entity.Diary;
+import com.odos.odos_server_v2.domain.story.dto.StoryDiarySummaryProjection;
 import java.time.LocalDateTime;
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import com.odos.odos_server_v2.domain.diary.entity.Diary;
-import com.odos.odos_server_v2.domain.story.dto.StoryDiarySummaryProjection;
 
 @Repository
 public interface StoryRepository extends JpaRepository<Diary, Long> {
